d59d0a82bdb3fee08f8202d1b610638d73fe13f1
lucas <lucas_fernandez@orange.fr>
2019-05-26 11:47:29 +0200

    view_*

---
+++ Content/hearder.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<title>On_conception</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="header.css">
+
+<header>
+    <nav>
+        <ul>
+            <li class="logo"><a href="?controller=home&action=home_page"><img src="image/logo_on.png" alt="logo" /></a></li>
+            <li><a href="?controller=home&action=agence">L'AGENCE</a></li>
+            <li><a href="?controller=home&action=real">REALISATIONS</a></li>
+            <li><a href="?controller=home&action=job">NOTRE JOB</a></li>
+            <li><a href="?controller=home&action=news">NEWS</a></li>
+            <li><a href="?controller=home&action=contact">CONTACT</a></li>
+            <li class="logo-moblie"><a href="#"><img src="image/logo_on.png" alt="logo" /></a></li>
+            <li class="nav-small"><a href="#"><img src="" alt="" /></a></li>
+        </ul>
+    </nav>
+</header>
---
+++ Views/view_job.php
@@ -0,0 +1,1 @@
+<?php include('Content/header.php'); ?>
