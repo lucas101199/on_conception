9dfada08d8714228c6d7d4da6bd24528894875e8
lucas <lucas_fernandez@orange.fr>
2019-05-29 01:41:26 +0200

    fin navbar

--- agence.php
+++ agence.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="fr">
   <head>
-    <title>agence</title>
+    <title>AGENCE</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,9 +10,41 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
   </head>
 
-  <?php include("header.php"); ?>
+  <header>
+    <nav class="navbar">
+    <div class="topnav">
+        <a href="home.php"><img src="image/logo_on.png" alt="logo" /></a>
+        <a href="agence.php" class="active">L'AGENCE</a>
+        <svg width="31" height="31">
+            <rect x="15" y="-5" width="10" height="10" transform="rotate(45)" />
+        </svg>
+        <a href="real.php">REALISATIONS</a>
+        <svg width="31" height="31">
+            <rect x="15" y="-5" width="10" height="10" transform="rotate(45)" />
+        </svg>
+        <a href="job.php">NOTRE JOB</a>
+        <svg width="31" height="31">
+            <rect x="15" y="-5" width="10" height="10" transform="rotate(45)" />
+        </svg>
+        <a href="news.php">NEWS</a>
+        <svg width="31" height="31">
+            <rect x="15" y="-5" width="10" height="10" transform="rotate(45)" />
+        </svg>
+        <a href="contact.php">CONTACT</a>
+    </div>
+    <div class="topnav-right">
+        <a href="https://www.youtube.com/channel/UCjq0mQHVEfTHfHK3Ml8xhcg"><i class="fab fa-youtube"></i></a>
+        <a href="https://www.instagram.com/on.conception/"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-facebook-square"></i></a>
+        <a href="#"><i class="fab fa-whatsapp"></i></a>
+    </div>
+    </nav>
+
+  </header>
+
   <br />
   <br />
 
--- home.php
+++ home.php
@@ -1,5 +1,54 @@
-<?php include("header.php"); ?>
+<!doctype html>
+<html lang="fr">
+  <head>
+    <title>ON CONCEPTION</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- <link rel="stylesheet" href="header.css"> -->
 
-<body>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+  </head>
 
+  <header>
+    <nav class="navbar">
+    <div class="topnav">
+        <a href="home.php"><img src="image/logo_on.png" alt="logo" /></a>
+        <a href="agence.php">L'AGENCE</a>
+        <svg width="31" height="31">
+            <rect x="15" y="-5" width="10" height="10" transform="rotate(45)" />
+        </svg>
+        <a href="real.php">REALISATIONS</a>
+        <svg width="31" height="31">
+            <rect x="15" y="-5" width="10" height="10" transform="rotate(45)" />
+        </svg>
+        <a href="job.php">NOTRE JOB</a>
+        <svg width="31" height="31">
+            <rect x="15" y="-5" width="10" height="10" transform="rotate(45)" />
+        </svg>
+        <a href="news.php">NEWS</a>
+        <svg width="31" height="31">
+            <rect x="15" y="-5" width="10" height="10" transform="rotate(45)" />
+        </svg>
+        <a href="contact.php">CONTACT</a>
+    </div>
+    <div class="topnav-right">
+        <a href="https://www.youtube.com/channel/UCjq0mQHVEfTHfHK3Ml8xhcg"><i class="fab fa-youtube"></i></a>
+        <a href="https://www.instagram.com/on.conception/"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-facebook-square"></i></a>
+        <a href="#"><i class="fab fa-whatsapp"></i></a>
+    </div>
+    </nav>
+
+  </header>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
+</html>
