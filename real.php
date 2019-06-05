9362342086b97d30254ead9f88660f7c04778d7d
lucas <lucas_fernandez@orange.fr>
2019-06-05 13:33:09 +0200

    2mov

--- real.php
+++ real.php
@@ -21,9 +21,9 @@
 
     <div id="myBtnContainer">
       <button class="btn active" onclick="filterSelection('all')"> ALL</button>
-      <button class="btn" onclick="filterSelection('nature')"> FILMS</button>
-      <button class="btn" onclick="filterSelection('cars')"> MOTION</button>
-      <button class="btn" onclick="filterSelection('people')"> PHOTOS</button>
+      <button class="btn" onclick="filterSelection('film')"> FILMS</button>
+      <button class="btn" onclick="filterSelection('motion')"> MOTION</button>
+      <button class="btn" onclick="filterSelection('photos')"> PHOTOS</button>
     </div>
 
     <div class="contenu">
@@ -47,11 +47,120 @@
                 <source src="image/Realisations/DM Performance FILM.mov">
             </video>
         </div>
+        <div class="photo">
+            <video width="400" height="190" controls>
+                <source src="image/Realisations/Evo Luxury Joaillerie FILM.mov">
+            </video>
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/Evoluxury 2 PHOTO.png">
+        </div>
+        <div class="photo">
+            <video width="400" height="190" controls>
+                <source src="image/Realisations/Evoluxury Full Version FILM.mov">
+            </video>
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/Evoluxury PHOTO.png">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/Horlogerie PHOTO.jpeg">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/Impala PHOTO copie.jpeg">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/Impala PHOTO.jpeg">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/L&JR 2 PHOTO.png">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/L&JR 3 PHOTO.png">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/L&JR 4 PHOTO.png">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/L&JR 5 PHOTO.png">
+        </div>
+        <div class="photo">
+            <video width="400" height="190" controls>
+                <source src="image/Realisations/L&JR Baselworld 2019 FILM.mov">
+            </video>
+        </div>
+        <div class="photo">
+            <video width="400" height="190" controls>
+                <source src="image/Realisations/L&JR MOTION.mp4">
+            </video>
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/L&JR PHOTO.png">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/Motors &co PHOTO copie.jpeg">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/Motors &co PHOTO.jpeg">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/Paris PHOTO.jpeg">
+        </div>
+        <div class="photo">
+            <video width="400" height="190" controls>
+                <source src="image/Realisations/Slow Motion MOTION.mp4">
+            </video>
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/swiss wash 2 PHOTO.png">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/swiss wash 3 PHOTO.png">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/swiss wash 4 PHOTO.png">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/swiss wash 5 PHOTO.png">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/swiss wash 6 PHOTO.png">
+        </div>
+        <div class="photo">
+            <video width="400" height="190" controls>
+                <source src="image/Realisations/Swiss Wash FILM.mov">
+            </video>
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/swiss wash PHOTO.png">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/SWISSWash 7 PHOTO.jpg">
+        </div>
+        <div class="photo">
+            <img src="image/Realisations/SWISSWash 8 PHOTO.jpg">
+        </div>
+        <div class="photo">
+            <video width="400" height="190" controls>
+                <source src="image/Realisations/Teaser Swiss Wash FILM.mp4">
+            </video>
+        </div>
+        <div class="photo">
+            <video width="400" height="190" controls>
+                <source src="image/Realisations/Viquo Deco FILM.mp4">
+            </video>
+        </div>
+        <div class="photo">
+            <video width="400" height="190" controls>
+                <source src="image/Realisations/Your Concept By MOTION.mov">
+            </video>
+        </div>
     </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="real.js"></script>
 </body>
 </html>
